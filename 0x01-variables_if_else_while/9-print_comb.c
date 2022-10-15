c7109accf4314b5290003b069f0e0bec34c0e269
Elite-AJ <ajayidavidanointing@gmail.com>
2022-10-14 21:35:10 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,12 @@ int main(void)
 	int n;
 
 	for (n = 30; n < 40; n++)
-
 		putchar(n);
-		if (n != 39)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-
+	if (n != 39)
+	{
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
