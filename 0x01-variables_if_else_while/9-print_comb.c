5a4fcdf7a7914591708ac120bf4c9752c0646d7a
Elite-AJ <ajayidavidanointing@gmail.com>
2022-10-14 21:28:54 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/**
+ * main -  prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 30; n < 40; n++)
+
+		putchar(n);
+		if (n != 39)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	putchar('\n');
+	return (0);
+}
