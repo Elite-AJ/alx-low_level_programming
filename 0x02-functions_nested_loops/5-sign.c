9389797d8787981364c3ca574edcd356376f62ca
Elite-AJ <ajayidavidanointing@gmail.com>
2022-10-19 16:21:37 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign -> prints the sign of a number either +, - or 0
+ * @n: any integer
+ * Return: return either 1,0,-1 conditionally
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+}
