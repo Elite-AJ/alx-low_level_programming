f26a7a38bccf4e6788e387906530ccf403329546
Elite-AJ <ajayidavidanointing@gmail.com>
2022-10-24 21:49:37 -0700

    9

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array -  prints n elements of an array of integers
+ * @a: pointer 1
+ * @n: pointer 2
+ * Return: Always 0
+ */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+		printf(",");
+	}
+	printf('\n');
+}
