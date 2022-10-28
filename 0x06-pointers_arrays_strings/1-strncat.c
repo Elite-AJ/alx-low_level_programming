7f6b38f9c9a885ce4e7084581f405d920fb0d58a
EliteAJ <ajayidavidanointing@gmail.com>
2022-10-28 06:01:01 +0100

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * -strncat - concatenates two strings.
+ *  @dest: copy to
+ *  @src: copy from 
+ *  @n: char 
+ *  Return: Return dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n &&  scr[j] != '\0')
+	{
+		dest[i] = src[j];
+		return (destiny);
+	}
+
