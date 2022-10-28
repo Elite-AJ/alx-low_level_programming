acf4e0c1713f5502ae2d8b0632c9547a83d85a7f
Elite-AJ <ajayidavidanointing@gmail.com>
2022-10-27 19:42:14 -0700

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: Return pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	i = 0;
+
+	while (dest[1] != '\0')
+	{
+
+		i++
+	}
+
+	j = 0;
+
+	while (scr[b] != '\0')
+	{
+		dest[a]  = src[b];
+
+		a++;
+		b++;
+	}
+
+	dest[a] != '\0'
+
+	return (dest)
+}
+
