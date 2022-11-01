fad89b723c82005c848072bf671cdef91f275aeb
Elite-AJ <ajayidavidanointing@gmail.com>
2022-10-31 20:19:07 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,14 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] >= '\0'; i++)
+	while (*s != '\0')
 	{
-		if (s[index] == c)
-			return (s + i);
+		if (*s == c)
+			return (s);
+		s++
 	}
 
+	if (*s == c)
+		return (s);
 	return ('\0');
 }
