7317aafc52dabb70d399496c28f86d376318c17b
Elite-AJ <ajayidavidanointing@gmail.com>
2022-10-31 18:44:20 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: pointer char
+ * @c: char input
+ * Return: s or 0 conditionally
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else
+			return (0);
+	}
+	return ('\0');
+}
