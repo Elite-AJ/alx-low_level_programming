0b2f1cbdd642978e671f1278b6f011ecdf22940f
Elite-AJ <ajayidavidanointing@gmail.com>
2022-11-03 00:55:59 -0700

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: char input
+ * Return: Return (s)
+ */
+
+void _puts_recursion(char *s)
+{
+	char a;
+
+	printf("%c/n", a);
+
+	return (s);
+}
