f7e09c8f08437b3d9f89bf7320a4ba80b364b5b8
Elite-AJ <ajayidavidanointing@gmail.com>
2022-11-03 01:42:51 -0700

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,10 +9,12 @@
 int _strlen_recursion(char *s)
 {
 	int len = 0;
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
+	if (*s)
+
+		{
+			len++;
+			len += _strlen_recursion(s + 1);
+		}
 	return (len);
 }
 
