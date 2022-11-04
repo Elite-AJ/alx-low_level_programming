6fe3c401c97fbf866da0b9209bb2228f3e635a22
Elite-AJ <ajayidavidanointing@gmail.com>
2022-11-03 20:30:16 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -20,7 +20,7 @@ int is_prime(unsigned int n, unsigned int c)
 }
 
 /**
- * is_prime - detects if an input number is a prime number
+ * is_prime_number - detects if an input number is a prime number
  * @n: input number
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
