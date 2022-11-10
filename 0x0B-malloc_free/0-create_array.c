5b3374bf28e625290c76a25275e92dac617500c0
Elite-AJ <ajayidavidanointing@gmail.com>
2022-11-09 19:22:56 -0800

    0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *create_array - creates an array of chars, and initializes it
+ * @size: number of array
+ * @c: char value
+ * Return: pointer location
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+	char *b;
+	unsigned int p;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+		p = 0;
+
+		while (p < size)
+		{
+			*(b + p) = c;
+			p++;
+		}
+		return (p);
+	}
+}
