90b4e28c0e6b0915883f83f274cc53e149dad7d0
Elite-AJ <ajayidavidanointing@gmail.com>
2022-11-15 00:56:23 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -35,7 +35,7 @@ void times_table(void)
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
-				if (b < c)
+				if (b < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -45,4 +45,3 @@ void times_table(void)
 		_putchar('\n');
 	}
 }
-
