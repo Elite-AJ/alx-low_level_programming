7cc11ee1b969a999a93d1003f756b657f36f109e
Elite-AJ <ajayidavidanointing@gmail.com>
2022-11-14 18:43:31 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,15 @@ int main(void)
 	int n;
 
 	for (n = 30; n < 40; n++)
-		putchar(n);
-	if (n != 39)
 	{
-		putchar(',');
-		putchar(' ');
+		putchar(n + '0');
+		if (n < 39)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+
 	putchar('\n');
 	return (0);
 }
