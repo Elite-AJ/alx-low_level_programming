62e1b112b11bde6da93a3bd4c1fc274520e2000e
Elite-AJ <ajayidavidanointing@gmail.com>
2022-11-15 07:15:19 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - sun two numbers and produce result
+ * @a: Digi1
+ * @b: Digi2
+ * Return: c
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
+
+
