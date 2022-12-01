a4101c52b4408c951f4a6d4a7623982c77990ec2
Elite-AJ <ajayidavidanointing@gmail.com>
2022-12-01 10:12:09 +0100

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/** 
+ * get_nodeint_at_index - get the nh node of listint_t
+ * @head: head of the list
+ * @index: position of the node
+ * Return: the nth node of the list else NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int p;
+
+	if (index == 0 && head)
+		return (head);
+
+	for (p = 0; head && p < index; p++)
+		head = head->next;
+
+	if (head)
+		return (head);
+	return (NULL);
+}
