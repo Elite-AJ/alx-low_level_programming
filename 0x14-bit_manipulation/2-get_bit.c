9781a337d8af796c14e5f879756e8b1e1ed273ee
Elite-AJ <ajayidavidanointing@gmail.com>
2022-12-01 15:52:31 +0100

    All task

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,13 +4,13 @@
  * get_bit - get bit at nth index
  * @n: binary number
  * @index: nth position
- * Return:  bit at that position 
+ * Return:  bit at that position
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index .= sizeof(n) * 8)
-		retiuen (-1);
+	if (index >= sizeof(n) * 8)
+		return (-1);
 
 	return ((n >> index) & 1);
 }
