6fe0ff179e7fee725215a683d37732d387474c57
Elite-AJ <ajayidavidanointing@gmail.com>
2022-12-01 15:45:17 +0100

    All task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - check the endian status
+ *
+ * Return: 1 if big and 0 otherwise
+ */
+
+int get_endianness(void)
+{
+	int x= 1;
+	char *c = (char *)(&x);
+
+	return (*c);
+}
