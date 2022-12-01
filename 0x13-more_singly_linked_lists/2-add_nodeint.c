9666e388a3d789626e38f030223d2b32795ce380
Elite-AJ <ajayidavidanointing@gmail.com>
2022-12-01 09:34:38 +0100

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,16 +4,16 @@
  * add_nodeint - add node at the begginging
  * @head: pointer head
  * @n: interger input
- * return: pointer to the list if successful else NULL
+ * Return: pointer to the list if successful else NULL
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
 
-	if(!new_node)
+	if (!new_node)
 		return (NULL);
-	new_node-> = n;
+	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
 
