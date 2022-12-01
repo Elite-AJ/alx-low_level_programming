b2e2dfd67d8a7ff6cff6288013085d0b82b0ab9a
Elite-AJ <ajayidavidanointing@gmail.com>
2022-12-01 10:47:58 +0100

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	while (index != 1)
 	{
 		if (current->next == NULL)
-			return (1);
+			return (-1);
 
 		current = current->next;
 		--index;
