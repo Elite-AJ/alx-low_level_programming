f0992b4d43a4d0961af64fa9ed31b3f398726cb1
Elite-AJ <ajayidavidanointing@gmail.com>
2022-12-26 10:09:53 +0100

    3err

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date, prints out the days left int that year
+ * printf_remaining_days - takes a date, prints out the days left int that year
  * @month: ...
  * @day: ...
  * @year: ...
  * Return: void
  */
 
-void print_remaining_days(int month, int day, int year)
+void printf_remaining_days(int month, int day, int year)
 {
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
