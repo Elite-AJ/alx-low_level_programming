c725ae03b92827163eb26ece1295e608a052e0fa
Elite-AJ <ajayidavidanointing@gmail.com>
2022-12-30 14:16:54 +0100

    1-corr

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,13 +19,13 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
-			len++
+			len++;
 	}
 
-	op = open(filename, O_CREAT | O_RDWR | 0_TRUNC, 0600);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (op == -1 || wr == -1)
 		return (-1);
 
 	close(op);
