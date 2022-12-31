318569a6bea5ce0691a37cb6dc5cc1f5303cdfc9
Elite-AJ <ajayidavidanointing@gmail.com>
2022-12-31 16:57:53 +0100

    10-corr

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,7 +4,7 @@
  * print_triangle - Entry point
  * Description: prints diagonals
  * @size: Size of the triangle
- * 
+ *
  * Return: Void
  */
 
@@ -20,7 +20,7 @@ void print_triangle(int size)
 	{
 		for (row = 1; row <= size; row++)
 		{
-			for (spaces = size -row; spaces >= 1; spaces--)
+			for (spaces = size - row; spaces >= 1; spaces--)
 			{
 				_putchar(' ');
 			}
